b1e152464e22087c9aa0786e08d2c836b51a3307
Filip Rydzi <rydzi.filip@gmail.com>
2019-04-07 21:57:19 +0200

    added gitignore

--- experiments_knowledge_service/dist/main/model/data_models.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * Representation of DTOs as they are stored in a Database.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- experiments_knowledge_service/dist/main/validation/ValidationException.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ValidationException extends Error {
-    constructor(message) {
-        super(message);
-        this.message = message;
-    }
-}
-exports.ValidationException = ValidationException;
